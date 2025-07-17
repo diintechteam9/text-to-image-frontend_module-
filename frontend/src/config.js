0e3eaec9f9fd9340ff450021e474128a12ec28d4
diintechteam9 <diintechteam9@gmail.com>
2025-07-17 11:52:38 +0530

    fourth commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // Base URL for all API calls
-// export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "http://localhost:4000";
 
-export const API_BASE_URL = "https://text-to-image-frontend-module.onrender.com";
+// export const API_BASE_URL = "https://text-to-image-frontend-module.onrender.com";
 
